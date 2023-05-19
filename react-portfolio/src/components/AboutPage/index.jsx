d67dc7e99a2316b3a733deb563adb04a87e2521f
jbroom86 <jbroom86@gmail.com>
2023-05-18 18:51:53 -0700

    added about me content

--- react-portfolio/src/components/AboutPage/index.jsx
+++ react-portfolio/src/components/AboutPage/index.jsx
@@ -1,9 +1,44 @@
 function AboutPage() {
-    return (
-        <>
-        <h1> This will be the About Page!</h1>
-        </>
-    )
+  return (
+    <>
+      <div>
+        <div className="flex justify-center items-center h-screen">
+          <div className="relative w-4/5 p-8">
+            <h1>About Me</h1>
+            <p class="bio">
+              Jeremiah is a skilled bassoonist with over 22 years of experience
+              in the field. He was born and raised in Tacoma, Washington, where
+              he developed his passion for music at a young age. After
+              graduating from high school, he decided to pursue his dream of
+              becoming a professional musician and enrolled in the San Francisco
+              Conservatory of Music.
+              <br  />
+              During his time at the conservatory, Jeremiah studied under some
+              of the most renowned bassoonists in the country and honed his
+              skills on the instrument. He also had the opportunity to perform
+              with a variety of ensembles and orchestras, both in San Francisco
+              and beyond.
+              <br />
+              After graduation, Jeremiah began his career as a professional
+              bassoonist and quickly established himself as a sought-after
+              performer. He has played with orchestras all over the world and
+              has collaborated with many of the most respected musicians in the
+              field.
+              <br />
+              Despite his success as a musician, Jeremiah has recently decided
+              to pursue a career change and is embarking on a journey into the
+              world of coding. He is excited to learn new skills and explore new
+              opportunities in this exciting field.
+              <br />
+              When he's not playing the bassoon or learning to code, Jeremiah
+              enjoys spending time with his two French bulldogs, Moo and Yakult.
+              They love going on walks together and taking naps on the couch.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default AboutPage;
