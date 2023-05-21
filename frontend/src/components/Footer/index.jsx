10b87901dfb530cdcdc8c2049ac18e3d7de34808
jbroom86 <jbroom86@gmail.com>
2023-05-21 14:28:15 -0700

    changed homepage to be more responsive

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -6,15 +6,22 @@ function Footer() {
       <Link to="https://github.com/JBroom86">
         <img
           src="https://seeklogo.com/images/G/github-logo-2E3852456C-seeklogo.com.png"
-          className="h-12 ml-auto bg-black"
-          alt="First Image"
+          className="h-12 ml-auto bg-black rounded-full"
+          alt="Jeremiah's GitHub"
         />
       </Link>
       <Link to="https://www.linkedin.com/in/jeremiahbroom/">
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/LinkedIn_icon_circle.svg/800px-LinkedIn_icon_circle.svg.png"
           className="h-12 "
-          alt="Second Image"
+          alt="Jeremiah's LinkedIn"
+        />
+      </Link>
+      <Link to="mailto:jbroom86@gmail.com">
+        <img
+          src="https://media.istockphoto.com/id/1248990173/vector/glowing-neon-line-paper-plane-icon-isolated-on-black-background-paper-airplane-icon-aircraft.jpg?s=170667a&w=0&k=20&c=P8aZfrPOc3qqIUWSrf8Z4cd2fzmWVhPzg0NCIz3zyXQ="
+          className="h-12 rounded-full"
+          alt="Email Jeremiah"
         />
       </Link>
     </footer>
--- frontend/src/components/HomePage/index.jsx
+++ frontend/src/components/HomePage/index.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
-    <div className="flex justify-center items-start h-full">
-      <div className="flex flex-col items-center mt-40">
+    <div className="flex justify-center items-start h-full sm:pb-10 sm:pt-1">
+      <div className="flex flex-col items-center sm:mt-40">
         <Link to="/about">
         <div className="relative group">
           <div className="
@@ -34,50 +34,54 @@ function HomePage() {
           Welcome to my page! My name is Jeremiah and I am a full stack
           developer located in Tacoma, WA.
         </p>
-        <div id="techstack" className="flex items-center mt-4 -my-80">
+        <div id="techstack" className="flex flex-wrap items-center mt-4 -my-80">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/539px-React-icon.svg.png"
-            className="h-10 w-10 mr-4"
+            className="h-10 w-10 sm:h-12 sm:w-12 mr-4 mb-4"
           />
           <img
             src="https://www.svgrepo.com/show/353657/django-icon.svg"
-            className="h-10 w-10 mr-4"
+            className="h-10 w-10 sm:h-12 sm:w-12 mr-4 mb-4"
           />
           <img
             src="https://cdn3.iconfinder.com/data/icons/logos-and-brands-adobe/512/267_Python-512.png"
-            className="h-10 w-10 mr-4"
+            className="h-10 w-10 sm:h-12 sm:w-12 mr-4 mb-4"
           />
           <img
             src="https://cdn-icons-png.flaticon.com/512/1199/1199124.png"
-            className="h-10 w-10 mr-4"
+            className="h-10 w-10 sm:h-12 sm:w-12 mr-4 mb-4"
           />
           <img
             src="https://cdn.cdnlogo.com/logos/c/18/css.svg"
-            className="h-10 w-10 mr-4"
+            className="h-10 w-10 sm:h-12 sm:w-12 mr-4 mb-4"
           />
           <img
             src="https://cdn-icons-png.flaticon.com/512/732/732212.png"
-            className="h-10 w-10 mr-4"
+            className="h-10 w-10 sm:h-12 sm:w-12 mr-4 mb-4"
           />
           <img
             src="https://cdn.freebiesupply.com/logos/large/2x/nodejs-icon-logo-png-transparent.png"
-            className="h-10 w-10 mr-4"
+            className="h-10 w-10 sm:h-12 sm:w-12 mr-4 mb-4"
           />
           <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/MongoDB_Logo.svg/2560px-MongoDB_Logo.svg.png"
-            className="h-10 w-18 mr-4"
+            src="https://w7.pngwing.com/pngs/956/695/png-transparent-mongodb-original-wordmark-logo-icon-thumbnail.png"
+            className="h-10 w-18 sm:h-12 sm:w-12 mr-4 mb-4 rounded "
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1985px-Postgresql_elephant.svg.png"
+            className="h-10 w-18 sm:h-12 sm:w-12 mr-4 mb-4 rounded "
           />
           <img
             src="https://static-00.iconduck.com/assets.00/tailwind-css-icon-2048x1229-u8dzt4uh.png"
-            className="h-10 w-12 mr-4"
+            className="h-10 w-12 sm:h-12 sm:w-12 mr-4 mb-4"
           />
           <img
             src="https://cdn.freebiesupply.com/logos/large/2x/bootstrap-4-logo-png-transparent.png"
-            className="h-10 w-10 mr-4"
+            className="h-10 w-10 sm:h-12 sm:w-12 mr-4 mb-4"
           />
           <img
             src="https://cdn.freebiesupply.com/logos/large/2x/sass-1-logo-png-transparent.png"
-            className="h-10 w-10 mr-4"
+            className="h-10 w-10 sm:h-12 sm:w-12 mr-4 mb-4"
           />
         </div>
       </div>
